c5db9a065d2a6b0e4f9f9a09ebafdf97e42499a8
jleng <jleng@ambarella.com>
2022-11-10 11:22:35 +0800

    [Build] Update build templates for safe copy
    Change-Id: I457950d93eeb40b90889bc27ac91768c23eaf57c

--- inc.app.mk
+++ inc.app.mk
@@ -4,6 +4,7 @@
 ##    2022/08/08 - [Jing Leng] Update to v1.0.0
 ##    2022/09/09 - [Jing Leng] Update to v1.1.0
 ##    2022/10/26 - [Jing Leng] Update to v1.2.0
+##    2022/11/10 - [Jing Leng] Add safe copy
 ##
 ## Copyright (c) 2022 Ambarella International LP
 ##
@@ -98,7 +99,6 @@ CFLAGS         += $(patsubst %,-I$(ENV_DEP_ROOT)/usr/include/%/,$(PACKAGE_DEPS) 
 LDFLAGS        += $(patsubst %,-L$(ENV_DEP_ROOT)%,/lib/ /usr/lib/ /usr/local/lib/)
 LDFLAGS        += $(patsubst %,-Wl$(comma)-rpath-link=$(ENV_DEP_ROOT)%,/lib/ /usr/lib/ /usr/local/lib/)
 endif
-
 
 ifeq ($(STRICT), y)
 CFLAGS         += -Wall # This enables all the warnings about constructions that some users consider questionable.
@@ -233,47 +233,51 @@ INSTALL_BINARIES ?= $(BIN_TARGETS)
 
 .PHONY: install_libs install_base_libs install_bins install_base_bins install_hdrs install_datas
 
+define safe_cp
+$(if $(filter yocto,$(ENV_BUILD_MODE)),cp $1 $2,flock $(ENV_INS_ROOT) -c "cp $1 $2")
+endef
+
 install_libs:
 	@install -d $(ENV_INS_ROOT)/usr/lib
-	@cp -drf $(INSTALL_LIBRARIES) $(ENV_INS_ROOT)/usr/lib
+	@$(call safe_cp,-drf,$(INSTALL_LIBRARIES) $(ENV_INS_ROOT)/usr/lib)
 
 INSTALL_BASE_LIBRARIES ?= $(INSTALL_LIBRARIES)
 install_base_libs:
 	@install -d $(ENV_INS_ROOT)/lib
-	@cp -drf $(INSTALL_BASE_LIBRARIES) $(ENV_INS_ROOT)/lib
+	@$(call safe_cp,-drf,$(INSTALL_BASE_LIBRARIES) $(ENV_INS_ROOT)/lib)
 
 install_bins:
 	@install -d $(ENV_INS_ROOT)/usr/bin
-	@cp -drf $(INSTALL_BINARIES) $(ENV_INS_ROOT)/usr/bin
+	@$(call safe_cp,-drf,$(INSTALL_BINARIES) $(ENV_INS_ROOT)/usr/bin)
 
 INSTALL_BASE_BINARIES ?= $(INSTALL_BINARIES)
 install_base_bins:
 	@install -d $(ENV_INS_ROOT)/bin
-	@cp -drf $(INSTALL_BASE_BINARIES) $(ENV_INS_ROOT)/bin
+	@$(call safe_cp,-drf,$(INSTALL_BASE_BINARIES) $(ENV_INS_ROOT)/bin)
 
 install_hdrs:
 	@install -d $(ENV_INS_ROOT)/usr/include/$(PACKAGE_NAME)
-	@cp -drfp $(INSTALL_HEADERS) $(ENV_INS_ROOT)/usr/include/$(PACKAGE_NAME)
+	@$(call safe_cp,-drfp,$(INSTALL_HEADERS) $(ENV_INS_ROOT)/usr/include/$(PACKAGE_NAME))
 
 install_datas:
 	@install -d $(ENV_INS_ROOT)/usr/share/$(PACKAGE_NAME)
-	@cp -drf $(INSTALL_DATAS) $(ENV_INS_ROOT)/usr/share/$(PACKAGE_NAME)
+	@$(call safe_cp,-drf,$(INSTALL_DATAS) $(ENV_INS_ROOT)/usr/share/$(PACKAGE_NAME))
 
 install_datas_%:
-	@icp="$(if $(findstring /include,$(lastword $(INSTALL_DATAS_$(patsubst install_datas_%,%,$@)))),cp -drfp,cp -drf)"; \
+	@icp="$(if $(findstring /include,$(lastword $(INSTALL_DATAS_$(patsubst install_datas_%,%,$@)))),-drfp,-drf)"; \
 		isrc="$(patsubst $(lastword $(INSTALL_DATAS_$(patsubst install_datas_%,%,$@))),,$(INSTALL_DATAS_$(patsubst install_datas_%,%,$@)))"; \
 		idst="$(ENV_INS_ROOT)/usr/share$(lastword $(INSTALL_DATAS_$(patsubst install_datas_%,%,$@)))"; \
-		install -d $${idst} && $${icp} $${isrc} $${idst}
+		install -d $${idst} && $(call safe_cp,$${icp},$${isrc} $${idst})
 
 install_todir_%:
-	@icp="$(if $(findstring /include,$(lastword $(INSTALL_TODIR_$(patsubst install_todir_%,%,$@)))),cp -drfp,cp -drf)"; \
+	@icp="$(if $(findstring /include,$(lastword $(INSTALL_TODIR_$(patsubst install_todir_%,%,$@)))),-drfp,-drf)"; \
 		isrc="$(patsubst $(lastword $(INSTALL_TODIR_$(patsubst install_todir_%,%,$@))),,$(INSTALL_TODIR_$(patsubst install_todir_%,%,$@)))"; \
 		idst="$(ENV_INS_ROOT)$(lastword $(INSTALL_TODIR_$(patsubst install_todir_%,%,$@)))"; \
-		install -d $${idst} && $${icp} $${isrc} $${idst}
+		install -d $${idst} && $(call safe_cp,$${icp},$${isrc} $${idst})
 
 install_tofile_%:
-	@icp="$(if $(findstring /include,$(lastword $(INSTALL_TOFILE_$(patsubst install_tofile_%,%,$@)))),cp -dfp,cp -df)"; \
+	@icp="$(if $(findstring /include,$(lastword $(INSTALL_TOFILE_$(patsubst install_tofile_%,%,$@)))),-dfp,-df)"; \
 		isrc="$(word 1,$(INSTALL_TOFILE_$(patsubst install_tofile_%,%,$@)))"; \
 		idst="$(ENV_INS_ROOT)$(lastword $(INSTALL_TOFILE_$(patsubst install_tofile_%,%,$@)))"; \
-		install -d $(dir $(ENV_INS_ROOT)$(lastword $(INSTALL_TOFILE_$(patsubst install_tofile_%,%,$@)))) && $${icp} $${isrc} $${idst}
+		install -d $(dir $(ENV_INS_ROOT)$(lastword $(INSTALL_TOFILE_$(patsubst install_tofile_%,%,$@)))) && $(call safe_cp,$${icp},$${isrc} $${idst})
 
