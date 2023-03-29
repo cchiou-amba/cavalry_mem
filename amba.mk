e6fcd9f136eda062143ed944594227cfe622bf2a
nick_dong <kdong@ambarella.com>
2023-03-29 17:12:21 +0800

    add pkgconfig pc file
    Signed-off-by: nick_dong <kdong@ambarella.com>
    Change-Id: I8409fc585bf37bf15de38b717a64592ad56c0b8d

--- amba.mk
+++ amba.mk
@@ -41,6 +41,7 @@ CAVALRY_MEMSO_VER_PATCH  := $(shell awk '/define $(CAVALRY_MEMVER_PREFIX)_LIB_PA
 LIBA_NAME = libcavalry_mem.a
 LIBSO_NAME = libcavalry_mem.so $(CAVALRY_MEMSO_VER_MAJOR) $(CAVALRY_MEMSO_VER_MINOR) $(CAVALRY_MEMSO_VER_PATCH)
 INSTALL_HEADERS = inc/*
+INSTALL_TODIR_pkgconfig = libcavalrymem.pc /usr/lib/pkgconfig
 
 SRC_PATH = src
 CFLAGS += $(shell echo -DCONFIG_ARCH_$(AMBA_SOC) | tr [:lower:] [:upper:])
@@ -59,5 +60,5 @@ clean: clean_objs
 	@-rm -f $(LIB_TARGETS)
 	@echo "Clean $(PACKAGE_NAME) Done."
 
-install: install_libs install_hdrs
+install: install_libs install_hdrs install_todir_pkgconfig
 
